2a7fd17bffa0a06aed3223a9cf4c32748ce4c897
Radu Farcas <f.radu10@yahoo.com>
2016-04-11 16:57:53 +0300

    modified header

--- js/components/header.js
+++ js/components/header.js
@@ -1,25 +1,106 @@
 import React from 'react';
 import ClassNames from 'classnames';
 
-import { Btn, AppBar, Block, Text, Utils, Menu, List, ListItem} from 'react-essence';
+import { AppBar, Block, Btn, Dialog, DialogHeader, DialogContent, DialogFooter, Divider, Icon, Image, Navigation, Text, Utils, Menu, List, ListItem,Switch } from 'react-essence';
+
+class HotelloNavigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: props.visible || false
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let states = this.state;
+    states.open = nextProps.visible;
+
+    this.setState(states);
+  }
+
+  render() {
+    return (
+      <Navigation className={'e-background-teal-A700'}visible={this.state.open}>
+      <Block className={'padding-left-20 e-margin-top-25 e-margin-bottom-25'}>
+        <Text className={'e-text-white e-title'}>
+          Caterit
+        </Text>
+      </Block>
+			<List type={'navigation'} className={'nav-ul e-body1'}>
+				<ListItem>
+				 <Text type={'a'}>
+					<Block classes={'content e-left e-text-white'}>MY ORDERS</Block>
+				 </Text>
+				</ListItem>
+				<Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
+			 <ListItem>
+				<Text type={'a'}>
+				 <Block classes={'content e-left e-text-white'}>ORDER HISTORY</Block>
+				</Text>
+			 </ListItem>
+			 <Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
+			 <ListItem>
+				<Text type={'a'}>
+				 <Block classes={'content e-left e-text-white'}>PAYMENT DETAILS</Block>
+				</Text>
+			 </ListItem>
+			 <Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
+			 <ListItem>
+				<Text type={'a'}>
+				 <Block classes={'content e-left e-text-white'}>DELIVERY ADDRESSES</Block>
+				</Text>
+			 </ListItem>
+			 <Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
+			 <ListItem>
+				<Text type={'a'}>
+				 <Block classes={'content e-left e-text-white'}>ABOUT</Block>
+				</Text>
+			 </ListItem>
+			 <Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
+			 <ListItem>
+				<Text type={'a'}>
+				 <Block classes={'content e-left e-text-white'}>LOG OUT</Block>
+				</Text>
+			 </ListItem>
+		 </List>
+       </Navigation>
+    );
+  }
+}
 
 class HotelloHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dialog: false,
+      navigation: false
+    };
+  }
 
-    render() {
-        return (
-        	<AppBar classes={'e-background-indigo-800'}>
-        		<Block className={'e-container '}>
-        			<Block className={'brick-12 e-no-margin'}>
-						<List type={'inline'}>
-							<ListItem>
-								<Text type={'a'} href={'#Home'} className={'e-text-uppercase e-text-white'}>Mobile</Text>
-							</ListItem>
-						</List>
-					</Block>
-				</Block>
-			</AppBar>
-        );
-    }
+  showNavigation() {
+    this.setState({
+    dialog: false,
+     navigation: true
+    });
+  }
+
+
+
+  render() {
+      return (
+        <Block classes={'e-text-white'}>
+          <AppBar className={'e-text-white e-background-grey-900 e-text-center'}>
+            <Btn icon={"navigation-menu"} className={"e-left e-text-white e-background-grey-900"} onClick={this.showNavigation.bind(this)} />
+
+            <Text className={'e-text-center e-text-white e-text-uppercase'}>hotello</Text>
+
+            <Btn icon={"action-search"} className={"e-text-white e-right e-background-grey-900"} />
+          </AppBar>
+          <HotelloNavigation visible={this.state.navigation} />
+        </Block>
+
+      );
+  }
 }
 
 module.exports = HotelloHeader;
