9624b4cdc43323a7877d4e1b703027f0b58ab710
radufarcas10 <f.radu10@yahoo.com>
2016-04-12 11:34:13 +0300

    finished header

--- js/components/header.js
+++ js/components/header.js
@@ -21,49 +21,49 @@ class HotelloNavigation extends React.Component {
   render() {
     return (
       <Navigation className={'e-background-teal-A700'}visible={this.state.open}>
-      <Block className={'padding-left-20 e-margin-top-25 e-margin-bottom-25'}>
-        <Text className={'e-text-white e-title'}>
-          Caterit
-        </Text>
-      </Block>
-			<List type={'navigation'} className={'nav-ul e-body1'}>
-				<ListItem>
-				 <Text type={'a'}>
-					<Block classes={'content e-left e-text-white'}>MY ORDERS</Block>
-				 </Text>
-				</ListItem>
-				<Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
-			 <ListItem>
-				<Text type={'a'}>
-				 <Block classes={'content e-left e-text-white'}>ORDER HISTORY</Block>
-				</Text>
-			 </ListItem>
-			 <Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
-			 <ListItem>
-				<Text type={'a'}>
-				 <Block classes={'content e-left e-text-white'}>PAYMENT DETAILS</Block>
-				</Text>
-			 </ListItem>
-			 <Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
-			 <ListItem>
-				<Text type={'a'}>
-				 <Block classes={'content e-left e-text-white'}>DELIVERY ADDRESSES</Block>
-				</Text>
-			 </ListItem>
-			 <Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
-			 <ListItem>
-				<Text type={'a'}>
-				 <Block classes={'content e-left e-text-white'}>ABOUT</Block>
-				</Text>
-			 </ListItem>
-			 <Divider classes={'thin e-background-grey-200 e-text-center adjust-divider'} />
-			 <ListItem>
-				<Text type={'a'}>
-				 <Block classes={'content e-left e-text-white'}>LOG OUT</Block>
-				</Text>
-			 </ListItem>
-		 </List>
-       </Navigation>
+        <Block className={'padding-left-20 e-margin-top-25 e-margin-bottom-25'}>
+          <Text className={'e-text-white e-title'}>
+            Caterit
+          </Text>
+        </Block>
+  			<List type={'navigation'} className={'nav-ul e-body1'}>
+  				<ListItem>
+  				 <Text type={'a'}>
+  					<Block classes={'content e-left e-text-white'}>MY ORDERS</Block>
+  				 </Text>
+  				</ListItem>
+  				<Divider classes={'thinnest e-background-grey-200 e-text-center'} />
+  			 <ListItem>
+  				<Text type={'a'}>
+  				 <Block classes={'content e-left e-text-white'}>ORDER HISTORY</Block>
+  				</Text>
+  			 </ListItem>
+  			 <Divider classes={'thinnest e-background-grey-200 e-text-center adjust-divider'} />
+  			 <ListItem>
+  				<Text type={'a'}>
+  				 <Block classes={'content e-left e-text-white'}>PAYMENT DETAILS</Block>
+  				</Text>
+  			 </ListItem>
+  			 <Divider classes={'thinnest e-background-grey-200 e-text-center adjust-divider'} />
+  			 <ListItem>
+  				<Text type={'a'}>
+  				 <Block classes={'content e-left e-text-white'}>DELIVERY ADDRESSES</Block>
+  				</Text>
+  			 </ListItem>
+  			 <Divider classes={'thinnest e-background-grey-200 e-text-center adjust-divider'} />
+  			 <ListItem>
+  				<Text type={'a'}>
+  				 <Block classes={'content e-left e-text-white'}>ABOUT</Block>
+  				</Text>
+  			 </ListItem>
+  			 <Divider classes={'thinnest e-background-grey-200 e-text-center adjust-divider'} />
+  			 <ListItem>
+  				<Text type={'a'}>
+  				 <Block classes={'content e-left e-text-white'}>LOG OUT</Block>
+  				</Text>
+  			 </ListItem>
+  		 </List>
+     </Navigation>
     );
   }
 }
@@ -88,13 +88,13 @@ class HotelloHeader extends React.Component {
 
   render() {
       return (
-        <Block classes={'e-text-white'}>
-          <AppBar className={'e-text-white e-background-grey-900 e-text-center'}>
-            <Btn icon={"navigation-menu"} className={"e-left e-text-white e-background-grey-900"} onClick={this.showNavigation.bind(this)} />
+        <Block classes={'hotello-header e-text-white'}>
+          <AppBar className={'e-text-white e-align-row e-background-grey-800 e-text-center'}>
+            <Btn icon={"navigation-menu"} className={"e-left e-text-grey-100 e-background-grey-800"} onClick={this.showNavigation.bind(this)} />
 
-            <Text className={'e-text-center e-text-white e-text-uppercase'}>hotello</Text>
+            <Text className={'e-text-center e-text-grey-100 e-headline e-text-uppercase'}>hotello</Text>
 
-            <Btn icon={"action-search"} className={"e-text-white e-right e-background-grey-900"} />
+            <Btn icon={"action-search"} className={"e-text-grey-100 e-right e-background-grey-800"} />
           </AppBar>
           <HotelloNavigation visible={this.state.navigation} />
         </Block>
