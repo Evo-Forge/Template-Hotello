c6471c04d21490b6ea39fb8a69eb69be8fdf9435
radufarcas10 <f.radu10@yahoo.com>
2016-04-20 12:25:36 +0300

    finished template

--- js/components/description.js
+++ js/components/description.js
@@ -123,7 +123,7 @@ class HotelloDescription extends React.Component {
         return (
           <Block classes={'hotello-description'}>
             <Block classes={'bg-big e-flex-container e-align-column e-space-between'}>
-              <Block classes={'e-align-row e-space-between'}>
+              <Block classes={'e-align-row header-adjust e-space-between'}>
                 <Block>
                   <Link to="/content/results">
                     <Btn icon={'hardware-keyboard-arrow-left'} ripple={true} type={'fab-mini'} className={'fab-mini btn-adjust e-text-grey-100 e-no-margin'} />
